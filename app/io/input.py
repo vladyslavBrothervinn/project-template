30cab86704b38f6c1328b1058e2808f1f8cf3f59
vladyslavBrothervinn <vladyslavbezb01@gmail.com>
2024-03-17 18:17:59 +0100

    empty functions

---
+++ app/io/input.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def input_from_keyboard():
+    pass
+
+
+def read_file():
+    pass
+
+
+def read_file_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,7 @@
+
+def to_print(string):
+    pass
+
+
+def write_to_file(text):
+    pass
